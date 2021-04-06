45216e8b51f4c6ada6038ef3ecaf786c094c461f
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-04-06 09:55:23 -0400

    implementing news page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 import "./App.css";
 import NewsContainer from "./components/NewsContainer";
+import NewsPage from "./components/NewsPage";
 
 function App() {
+  const [news, setNews] = useState(null);
+
   return (
     <div className="App">
-      <NewsContainer />
+      {news ? <NewsPage news={news} /> : <NewsContainer setNews={setNews} />}
     </div>
   );
 }
